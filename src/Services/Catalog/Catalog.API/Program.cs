fcfe91df1338dce7708e50de32ec5eb4f4049c6a
jules <jules.douglas@hotmail.com>
2026-05-21 23:23:02 -0600

    Setup and configure PostgreSQL environment on Docker

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,14 @@
+using Weasel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
-
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.AutoCreateSchemaObjects = AutoCreate.All;
+}).UseLightweightSessions();
 var app = builder.Build();
 
 app.MapCarter();
