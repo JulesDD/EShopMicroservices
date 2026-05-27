c98acfe40782c32c934d05e13fac4c0b7c1fbd79
jules <jules.douglas@hotmail.com>
2026-05-27 11:24:14 -0600

    Created a new Project with a few empty classes I will begin to work on implementing CQRS for checkout CRUD operations

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Models;
+
+//As the name implies. It's the model for an item in the cart
+public class ShoppingCartItem
+{
+    public int Quantity { get; set; } = default!;
+    public string Color { get; set; } = default!;
+    public decimal Price { get; set; } = default!;
+    public Guid ProductId { get; set; } = default!;
+    public string ProductName { get; set; } = default!;
+
+}
