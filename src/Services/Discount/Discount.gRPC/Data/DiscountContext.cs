69f654d173d75a24d3a20225de8bc5150972b43f
jules <jules.douglas@hotmail.com>
2026-06-01 17:19:22 -0600

    Create a Grpc class. Implemented sqlite and applied migrations

---
+++ src/Services/Discount/Discount.gRPC/Data/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using Discount.gRPC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+
+public class DiscountContext : DbContext 
+{
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone 17", Description = "IPhone Discount", Amount = 200 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+        );
+    }
+}
+
+
