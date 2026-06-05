417727a0e957a77a3d109cd2dfd753024d386f23
jules <jules.douglas@hotmail.com>
2026-06-05 12:06:52 -0600

    Developed EventHandler and Queries in Ordering.Application

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -8,3 +8,6 @@ global using Ordering.Domain.ValueObjects;
 global using FluentValidation;
 global using BuildingBlocks.Exceptions;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
