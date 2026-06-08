a04bab5d651b648d8981b83b73f7e56ee94b8ba9
jules <jules.douglas@hotmail.com>
2026-06-08 01:23:21 -0600

    Introduced Pagination for Orders

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+internal class GetOrdersByCustomerHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    //Get orders by customer using dbContext and return result
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+        
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
